4d14e5bba89c3924ef258780889ce221412b29ef
James MARTIN <cto@sparte.io>
2019-03-06 12:41:55 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/blog',
+    pathMatch: 'full'
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'skills',
+    component: BlogComponent
+  },
+  {
+    path: 'experience',
+    component: BlogComponent
+  },
+  {
+    path: 'about',
+    component: BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  MatToolbarModule,
+  MatListModule
+} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GravatarModule } from 'ngx-gravatar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +24,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatListModule,
     GravatarModule
   ],
   providers: [],
