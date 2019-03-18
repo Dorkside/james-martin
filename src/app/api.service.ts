3c973d7ffbaffde7b6aedc2559d2ee082949f500
James MARTIN <cto@sparte.io>
2019-03-18 18:33:41 +0100

    api service publication date

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -19,6 +19,7 @@ export class ApiService {
             edges {
               node {
                 id
+                publication
                 title
                 content
               }
