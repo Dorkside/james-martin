b3de9593e479dc6e0dd0f30f38bdf9e89bb1738f
James MARTIN <cto@sparte.io>
2019-04-03 23:39:38 +0200

    aniamte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,6 +33,7 @@ export class Drawing {
   currentPathIndex;
   leng;
   easeout;
+  reverse = false;
 
   constructor(timeBetweenDraws) {
     this.s = Snap("#svgout");
@@ -44,7 +45,13 @@ export class Drawing {
 
   init() {
     this.group.clear();
-    this.currentPathIndex = 0;
+    this.currentPathIndex = 1;
+  };
+
+  startReached() {
+    if (this.currentPathIndex <= 0) {
+      return true;
+    };
   };
 
   endReached() {
@@ -54,32 +61,47 @@ export class Drawing {
   };
 
   callOnFinished() {
+    this.currentPathIndex = 1;
+    this.reverse = !this.reverse;
   }
 
   draw = () => {
-    console.log(mina.easeout);
     if (this.endReached()) {
       if (this.callOnFinished) {
         this.callOnFinished();
-        return
+      };
+    };
+    if (this.startReached()) {
+      if (this.callOnFinished) {
+        this.s.clear();
+        this.callOnFinished();
       };
     };
     var myPath = this.pathArray[this.currentPathIndex];
 
     this.leng = myPath.getTotalLength();
 
-    this.group.append(myPath);
+    if (!this.reverse) {
+      this.group.append(myPath);
 
-    myPath.attr({
-      "fill": "none",
-      "stroke": "#000",
-      "strokeWidth": 3,
-      "stroke-dasharray": this.leng + " " + this.leng,
-      "stroke-dashoffset": this.leng
-    });
+      myPath.attr({
+        "stroke-dasharray": this.leng + " " + this.leng,
+        "stroke-dashoffset": this.leng
+      });
+
+      myPath.addClass('stroke');
+
+      myPath.animate({ "stroke-dashoffset": 0 }, this.leng * 3, mina.easeout, this.draw);
+
+      myPath.addClass('white');
+      myPath.removeClass('black');
+    } else {
+      myPath.addClass('black');
+      myPath.removeClass('white');
+
+      myPath.animate({ "stroke-dashoffset": this.leng }, this.leng, mina.easeout, this.draw);
+    }
 
     this.currentPathIndex++;
-
-    myPath.animate({ "stroke-dashoffset": 0 }, 10000, mina.easeout, this.draw);
   }
 }
